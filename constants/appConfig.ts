2fb07ac962a2c14898082953cefcf4d08b2071c1
Seo <seo@dayoff.co.kr>
2024-04-04 17:19:45 +0900

    added default meta tags

---
+++ constants/appConfig.ts
@@ -0,0 +1,30 @@
+const appConfig = {
+  domain: "https://www.venturesandvignettes.com",
+  title: "Ventures & Vignettes: Unraveling the world's tales",
+  description:
+    "Ventures & Vignettes offers a unique perspective on travel, weaving together the rich tapestry of history, culture, and the beauty of the world's destinations.",
+  keywords: [
+    "Travel Blog",
+    "Cultural Exploration",
+    "Historical Destinations",
+    "Adventure Travel",
+    "Travel Guides",
+    "City Breaks",
+    "Cultural Insights",
+    "Historical Tours",
+    "Artistic Getaways",
+    "Museum Tours",
+    "World Heritage Sites",
+    "Local Cuisine",
+    "Backpacking Adventures",
+    "Luxury Travel",
+    "Hidden Gems",
+    "Cultural Festivals",
+    "Travel Itineraries",
+    "Solo Travel",
+    "Sustainable Tourism",
+    "Travel Photography",
+  ],
+};
+
+export default appConfig;
