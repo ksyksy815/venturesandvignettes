b1b765de9873a7cddb53da23930f94f1a6b08c2b
Seo <seo@dayoff.co.kr>
2024-04-11 15:36:27 +0900

    logo capitalize

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div
         className={`flex items-center justify-between w-full max-w-5xl p-3 lg:px-6`}
       >
-        <p className={`text-6`}>Ventures & vignettes</p>
+        <p className={`text-6`}>Ventures & Vignettes</p>
         <button type={"button"}>
           <FiSearch size={24} />
         </button>
