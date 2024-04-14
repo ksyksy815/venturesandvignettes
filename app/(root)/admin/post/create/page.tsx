d70cdcb90bad8ae0a0907bf30f82fe43f2028746
Seoyeon Kim <seoyeonkim815@gmail.com>
2024-04-14 11:49:07 +0900

    editor page

--- app/(root)/admin/post/create/page.tsx
+++ app/(root)/admin/post/create/page.tsx
@@ -4,8 +4,15 @@ import Link from "next/link";
 
 const Page = () => {
   return (
-    <BasePage
-      className={`px-6 py-12 gap-y-6 lg:px-8 lg:py-16 lg:gap-y-8`}></BasePage>
+    <BasePage className={`px-6 py-12 gap-y-6 lg:px-8 lg:py-16 lg:gap-y-8`}>
+      <div className="w-full flex flex-col gap-y-8">
+        <div>tool bar</div>
+        <input type="text" className="h-12 w-full px-4" />
+        <div>editor</div>
+        <div>further meta info</div>
+        <div>save button</div>
+      </div>
+    </BasePage>
   );
 };
 
