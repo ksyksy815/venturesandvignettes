b7d9c2c93a072f0ce6ecb4897b2b134fbf11d998
Seo <seo@dayoff.co.kr>
2024-04-15 10:21:36 +0900

    temporarily disabled autch check for admin routes

--- app/(root)/admin/Auth.tsx
+++ app/(root)/admin/Auth.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { LOGIN_COOKIE } from "@/constants";
-import { redirect } from "next/navigation";
 import React from "react";
-import { useCookies } from "react-cookie";
 
 const Auth = ({ children }: { children: React.ReactNode }) => {
-  const [cookies] = useCookies([LOGIN_COOKIE]);
+  // Temporary disabled
+  // const [cookies] = useCookies([LOGIN_COOKIE]);
 
-  if (!cookies[LOGIN_COOKIE]) {
-    redirect("/login");
-  }
+  // if (!cookies[LOGIN_COOKIE]) {
+  //   redirect("/login");
+  // }
 
   return <>{children}</>;
 };
