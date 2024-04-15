4050d399b9ee1d6f7188a807892be9af3b8943cd
Seo <seo@dayoff.co.kr>
2024-04-15 10:33:39 +0900

    slightly changed the text editor design

--- app/(root)/admin/post/create/page.tsx
+++ app/(root)/admin/post/create/page.tsx
@@ -4,9 +4,12 @@ import BasePage from "@/components/shared/BasePage";
 const Page = () => {
   return (
     <BasePage className={`px-6 py-12 gap-y-6 lg:px-8 lg:py-16 lg:gap-y-8`}>
-      <div className="w-full flex flex-col gap-y-8">
-        <div>tool bar</div>
-        <input type="text" className="h-12 w-full px-4" />
+      <div className="w-full flex flex-col">
+        <input
+          type="text"
+          className="h-12 w-full px-4 text-2xl font-medium bg-transparent focus:outline-none placeholder:font-normal"
+          placeholder="Title"
+        />
         <MainEditor />
         <div>further meta info</div>
         <div>save button</div>
