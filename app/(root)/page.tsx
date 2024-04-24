9a918d729f1f231f268bf6db8949f72d205f47f9
Seo <seo@dayoff.co.kr>
2024-04-24 18:30:09 +0900

    Changed the medium of fetching latest post list from client to server

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import BasePage from "@/components/shared/BasePage";
-import { getFeaturedPosts } from "@/lib/actions/post.action";
+import { getAllBlogPosts, getFeaturedPosts } from "@/lib/actions/post.action";
 import { getAllTags } from "@/lib/actions/tag.action";
 import FeaturedSlider from "@/screens/main/FeaturedSlider";
 import Latest from "@/screens/main/Latest";
@@ -8,11 +8,15 @@ import TagSection from "@/screens/main/TagSection";
 export default async function Home() {
   const featuredPostList = await getFeaturedPosts();
   const tagList = await getAllTags();
+  const data = await getAllBlogPosts({
+    limit: 6,
+    page: 0,
+  });
 
   return (
     <BasePage>
       <FeaturedSlider list={featuredPostList} />
-      <Latest />
+      <Latest list={data?.data || []} />
       <TagSection list={tagList || []} />
     </BasePage>
   );
--- screens/main/Latest.tsx
+++ screens/main/Latest.tsx
@@ -2,18 +2,19 @@
 
 import PostList from "@/components/shared/PostList";
 import PostListSkeleton from "@/components/shared/PostListSkeleton";
-import useFetchPostList from "@/hooks/post/useFetchPostList";
+import { BlogPost } from "@/types/post.type";
 import { Suspense } from "react";
 
-const Latest = () => {
-  const { postList } = useFetchPostList();
-
+type Props = {
+  list: BlogPost[];
+};
+const Latest = ({ list }: Props) => {
   return (
     <section className={`w-full flex flex-col px-5 py-12 gap-y-6 lg:px-8`}>
       <h2 className={`h2`}>Latest Posts</h2>
 
       <Suspense fallback={<PostListSkeleton />}>
-        <PostList list={postList} />
+        <PostList list={list} />
       </Suspense>
     </section>
   );
