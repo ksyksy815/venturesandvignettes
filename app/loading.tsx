54e3aba09550c0e257973407db38ba8a63fc2212
Seo <seo@dayoff.co.kr>
2024-04-26 09:49:39 +0900

    Added page loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,10 @@
+import PageLoading from "@/components/shared/PageLoading";
+
 const Loading = () => {
-  return <div>loading</div>;
+  return (
+    <div className={`w-screen h-screen grid place-content-center bg-white`}>
+      <PageLoading />
+    </div>
+  );
 };
 export default Loading;
